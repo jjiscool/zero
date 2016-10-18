d1af70c9e981a85988a1ca3452482676bf298c7a
jimjiao <jimjiao@tencent.com>
2016-10-18 15:37:53 +0800

    Phase模块
    Phase模块

---
+++ client/UGame/Assets/PhaseHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Phase
+{	
+	public Phase(){
+		
+	}
+	virtual void PhaseAction(){
+		
+	}
+	virtual void PhaseUpdate(){
+		
+	}
+	
+}
+public class PhaseHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
