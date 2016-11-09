65bb1bf03f1b0cd5ff733760b670617516290950
bigKOLA <koladream@gmail.com>
2016-11-09 15:58:53 +0800

    sss
    sss

---
+++ client/UGame/Assets/scripts/lightCover.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lightCover : MonoBehaviour {
+
+
+	public Texture[] mask;
+
+	private float b = 0;
+	private float c = 0.1f;
+	private int i = 0;
+
+
+	void Cycle(){
+		if (Time.time > b) {
+			b = b + c;
+			if (i < mask.Length-1) {
+				i++;
+
+			} else {
+				i = 0;
+			}
+			GetComponent<SpriteRenderer>().material.SetTexture ("_Mask", mask[i]);
+
+		}
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Cycle ();
+	}
+}
