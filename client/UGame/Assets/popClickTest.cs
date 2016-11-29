f4ab39fb42ada4d93cd20bf82b01e34bc3a81a48
bigKOLA <koladream@gmail.com>
2016-11-29 17:48:48 +0800

    添加弹框（部分，不完全）
    添加弹框（部分，不完全）

---
+++ client/UGame/Assets/popClickTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+
+public class popClickTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.gameObject.GetComponent<Button>().onClick.AddListener(TestClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void TestClick(){
+		GameObject.Find ("popContainer").GetComponent<popController> ().fadeIn();
+	}
+}
---
+++ client/UGame/Assets/scripts/popController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class popController : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		transform.GetComponent<RectTransform> ().localScale = new Vector3 (0, 0, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void fadeIn(){
+		transform.GetComponent<RectTransform> ().localScale = new Vector3 (1, 1, 1);
+		transform.Find ("popCover").GetComponent<Animator> ().SetTrigger ("fadeIn");
+
+	}
+
+
+}
