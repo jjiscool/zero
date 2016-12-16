8d4f9280b8956a1488d627a43dab855bbd5c84ec
jjiscool <76481165@qq.com>
2016-12-16 09:22:40 +0800

    damagetext

--- client/UGame/Assets/scripts/deleteDamageText.cs
+++ client/UGame/Assets/scripts/deleteDamageText.cs
@@ -1,11 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using DG.Tweening;
+using UnityEngine.UI;
 public class deleteDamageText : MonoBehaviour {
-
 	// Use this for initialization
 	void Start () {
-	
+		
+		//debrisSequence.AppendCallback (() => Destroy (transform.gameObject));
+	}
+	void Cast(float x,float y){
+		
+		Sequence debrisSequence = DOTween.Sequence ();
+		RectTransform dt = this.GetComponent<Text> ().rectTransform;
+		dt.localPosition = new Vector3 (x, y, 0);
+		this.GetComponent<Text> ().color=new Color(1,1,1,0);
+		debrisSequence.Append (dt.DOLocalMoveY(100f,1));
+		debrisSequence.Join (dt.gameObject.GetComponent<Text>().DOFade(1.0f,0.3f));
+		debrisSequence.Append (dt.DOLocalMoveY(200f,1).SetDelay(0.8f));
+		debrisSequence.Join (dt.gameObject.GetComponent<Text>().DOFade(0,0.3f));
+		debrisSequence.AppendCallback (() => Destroy (transform.gameObject));
+		
 	}
 	
 	// Update is called once per frame
